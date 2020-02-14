c6fad0946f746a064264bcb264f34944ffefdc5c
杨山 <shan.yang@cyber-insight.com>
2020-02-14 17:01:53 +0800

    basic promise with state

--- frontend/promise.js
+++ frontend/promise.js
@@ -2,24 +2,29 @@
 
 class Promise {
   constructor(fn) {
+    this.state = "pending";
+    this.value = null;
     this.callbacks = [];
     fn(this._resolve.bind(this));
   }
 
-  then(callback) {
-    this.callbacks.push(callback);
+  then(onFulfilled) {
+    if (this.state === "pending") {
+      this.callbacks.push(onFulfilled);
+    } else {
+      onFulfilled(this.value);
+    }
     return this;
   }
 
   _resolve(value) {
-    setTimeout(() => {
-      this.callbacks.forEach(callback => callback(value));
-    });
+    this.state = "resolved";
+    this.value = value;
+    this.callbacks.forEach(callback => callback(value));
   }
 }
 
 let promise = new Promise(resolve => {
-  console.log(6);
   resolve("同步执行");
 });
 
