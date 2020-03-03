9f1446a52cc207046884b191c5cbedbe32fa6f75
andouillette-ys <yangshan0823@outlook.com>
2020-03-03 22:13:40 +0800

    rename folders

---
+++ frontend/string/isContain/index.js
@@ -0,0 +1,32 @@
+// 请使用最基本的遍历来实现判断字符串 a 是否被包含在字符串 b 中，并返回第一次出现的位置（找不到返回 -1）。
+
+// a = "34";
+// b = "1234567"; // 返回 2
+// a = "35";
+// b = "1234567"; // 返回 -1
+// a = "355";
+// b = "12354355"; // 返回 5
+// isContain(a, b);
+
+function isContain(a, b) {
+  if (typeof a !== "string" || typeof b !== "string") {
+    return -1;
+  }
+
+  for (let i = 0; i < b.length; i++) {
+    let result = true;
+    for (let j = 0; j < a.length; j++) {
+      if (a[j] !== b[i + j]) {
+        result = false;
+        break;
+      }
+    }
+    if (result) {
+      return i;
+    }
+  }
+
+  return -1;
+}
+
+module.exports = isContain;
