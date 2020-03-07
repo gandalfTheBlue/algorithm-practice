04d5cb15880b40ae17f83c3a368796015f768aa2
andouillette-ys <yangshan0823@outlook.com>
2020-03-07 17:08:54 +0800

    优化length of longest substring

--- leetcode/03-lengthOfLongestSubstring/index.js
+++ leetcode/03-lengthOfLongestSubstring/index.js
@@ -2,35 +2,58 @@
  * @param {string} s
  * @return {number}
  */
+// var lengthOfLongestSubstring = function(s) {
+//   if (!s || s.length === 0) return 0
+
+//   let result = 0
+//   let tempSubstring = ''
+
+//   for (let i = 0; i < s.length; i++) {
+//     tempSubstring = s[i]
+//     for (let j = i + 1; j < s.length; j++) {
+//       if (tempSubstring.indexOf(s[j]) < 0) {
+//         tempSubstring += s[j]
+//       } else {
+//         if (tempSubstring.length > result) {
+//           result = tempSubstring.length
+//         }
+//         tempSubstring = ''
+//         break
+//       }
+//       if (j === s.length - 1 && tempSubstring.length > result) {
+//         result = tempSubstring.length
+//       }
+//     }
+//   }
+
+//   if (tempSubstring.length > result) {
+//     return tempSubstring.length
+//   }
+
+//   return result
+// }
+
 var lengthOfLongestSubstring = function(s) {
   if (!s || s.length === 0) return 0
 
-  let result = 0
-  let tempSubstring = ''
-
+  //aabaab!bb
+  let start = 0
+  let maxLen = 0
+  let map = new Map()
   for (let i = 0; i < s.length; i++) {
-    tempSubstring = s[i]
-    for (let j = i + 1; j < s.length; j++) {
-      if (tempSubstring.indexOf(s[j]) < 0) {
-        tempSubstring += s[j]
-      } else {
-        if (tempSubstring.length > result) {
-          result = tempSubstring.length
-        }
-        tempSubstring = ''
-        break
-      }
-      if (j === s.length - 1 && tempSubstring.length > result) {
-        result = tempSubstring.length
-      }
+    const curr = s[i]
+    const isDuplicate = map[curr] > -1
+    const includeCurrent = isDuplicate && map[curr] >= start ? 0 : 1
+    if (i - start + includeCurrent > maxLen) {
+      maxLen = i - start + includeCurrent
     }
+    if (isDuplicate && map[curr] + 1 > start) {
+      start = map[curr] + 1
+    }
+    map[curr] = i
   }
 
-  if (tempSubstring.length > result) {
-    return tempSubstring.length
-  }
-
-  return result
+  return maxLen
 }
 
 module.exports = lengthOfLongestSubstring
--- leetcode/03-lengthOfLongestSubstring/index.test.js
+++ leetcode/03-lengthOfLongestSubstring/index.test.js
@@ -25,5 +25,17 @@ test('should return 2', () => {
 })
 
 test('should return 3', () => {
-  expect(lengthOfLongestSubstring('dvdf')).toBe(3)
+  expect(lengthOfLongestSubstring('dsvdf')).toBe(4)
+})
+
+test('should return 2', () => {
+  expect(lengthOfLongestSubstring('abba')).toBe(2)
+})
+
+test('should return 5', () => {
+  expect(lengthOfLongestSubstring('tmmzuxt')).toBe(5)
+})
+
+test('should return 3', () => {
+  expect(lengthOfLongestSubstring('aabaab!bb')).toBe(3)
 })
