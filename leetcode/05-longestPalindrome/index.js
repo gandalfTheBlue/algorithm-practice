7641e1d877db4859f6fa1fb2848b9899f0ade2b4
杨山 <shan.yang@cyber-insight.com>
2020-03-13 16:50:09 +0800

    重构longest palindrome

--- leetcode/05-longestPalindrome/index.js
+++ leetcode/05-longestPalindrome/index.js
@@ -3,30 +3,69 @@
  * @return {string}
  */
 // cbbd
-var longestPalindrome = function(s) {
-  if (!s || s === '') return s
+// var longestPalindrome = function(s) {
+//   if (!s || s === "") return s;
 
-  let map = new Map()
-  s.split('').forEach((char, index) => (map[index] = char))
-  for (let maxLen = s.length; maxLen > 0; maxLen--) {
-    for (let j = 0; j < s.length - maxLen + 1; j++) {
-      let isMaxLen = true
-      let start = j
-      let end = maxLen + j - 1
-      while (start < end) {
-        if (map[start] !== map[end]) {
-          isMaxLen = false
-          break
+//   let map = new Map();
+//   s.split("").forEach((char, index) => (map[index] = char));
+//   for (let maxLen = s.length; maxLen > 0; maxLen--) {
+//     for (let j = 0; j < s.length - maxLen + 1; j++) {
+//       let isMaxLen = true;
+//       let start = j;
+//       let end = maxLen + j - 1;
+//       while (start < end) {
+//         if (map[start] !== map[end]) {
+//           isMaxLen = false;
+//           break;
+//         }
+//         start++;
+//         end--;
+//       }
+//       if (isMaxLen) {
+//         return s.substring(j, maxLen + j);
+//       }
+//     }
+//   }
+//   return s[0];
+// };
+//aaabaaaa
+var longestPalindrome = function(s) {
+  if (!s || s === "") return s;
+
+  let result = s[0];
+  for (let i = 0; i < s.length; i++) {
+    let left = i;
+    let right = i + 1;
+    let isOddPalindrome = true;
+    let isEvenPalindrome = true;
+    while (isOddPalindrome || isEvenPalindrome) {
+      if (isOddPalindrome && s[left - 1] === s[right]) {
+        if (right - left + 2 > result.length) {
+          result = s.substring(left - 1, right + 1);
         }
-        start++
-        end--
       }
-      if (isMaxLen) {
-        return s.substring(j, maxLen + j)
+      if (isEvenPalindrome && s[left] === s[right]) {
+        if (right - left + 1 > result.length) {
+          result = s.substring(left, right + 1);
+        }
       }
+
+      if (isOddPalindrome) {
+        if (s[left - 1] !== s[right] || left === 0 || right === s.length - 1) {
+          isOddPalindrome = false;
+        }
+      }
+      if (isEvenPalindrome) {
+        if (s[left] !== s[right] || left === 0 || right === s.length - 1) {
+          isEvenPalindrome = false;
+        }
+      }
+
+      left--;
+      right++;
     }
   }
-  return s[0]
-}
+  return result;
+};
 
-module.exports = longestPalindrome
+module.exports = longestPalindrome;
--- leetcode/05-longestPalindrome/index.test.js
+++ leetcode/05-longestPalindrome/index.test.js
@@ -1,13 +1,21 @@
-const longestPalindrome = require('.')
+const longestPalindrome = require(".");
 
-test('should return ababa', () => {
-  expect(longestPalindrome('ababa')).toBe('ababa')
-})
+test("should return ababa", () => {
+  expect(longestPalindrome("ababa")).toBe("ababa");
+});
 
-test('should return a', () => {
-  expect(longestPalindrome('a')).toBe('a')
-})
+test("should return a", () => {
+  expect(longestPalindrome("a")).toBe("a");
+});
 
-test('should return bb', () => {
-  expect(longestPalindrome('cbbd')).toBe('bb')
-})
+test("should return bb", () => {
+  expect(longestPalindrome("cbbd")).toBe("bb");
+});
+
+test("should return dasad", () => {
+  expect(longestPalindrome("dffsdasad")).toBe("dasad");
+});
+
+test("should return aaabaaa", () => {
+  expect(longestPalindrome("aaabaaaa")).toBe("aaabaaa");
+});
